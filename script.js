216c0b31ad92f8f5dcd7a89f5971d3833af55417
luvitamelati02 <melatiluvita31@gmail.com>
2025-08-17 22:04:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-function createGlitter() {
-  const glitter = document.createElement("div");
-  glitter.classList.add("glitter");
-  glitter.style.left = Math.random() * window.innerWidth + "px";
-  glitter.style.animationDuration = 3 + Math.random() * 5 + "s";
-  document.body.appendChild(glitter);
+function createGlitters(amount) {
+  for (let i = 0; i < amount; i++) {
+    const glitter = document.createElement("div");
+    glitter.classList.add("glitter");
 
-  setTimeout(() => {
-    glitter.remove();
-  }, 8000);
+    // posisi random
+    glitter.style.left = Math.random() * window.innerWidth + "px";
+
+    // durasi random biar beda-beda
+    glitter.style.animationDuration = 3 + Math.random() * 5 + "s";
+
+    // delay random biar ga jatuh bareng
+    glitter.style.animationDelay = Math.random() * 5 + "s";
+
+    document.body.appendChild(glitter);
+  }
 }
 
-setInterval(createGlitter, 200);
+// bikin 30 glitter aja (cukup, biar ga berat di HP)
+createGlitters(30);
